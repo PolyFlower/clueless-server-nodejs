075240181f62794e037899af8f08e4e851d34f80
Roman Avdeenko <btchead@protonmail.ch>
2022-06-28 00:47:03 +0300

    feat: httpException new property body * interface for body shape

--- common/exceptions/http.exception.ts
+++ common/exceptions/http.exception.ts
@@ -1,11 +1,26 @@
 import { HttpStatus } from '@common/enums/http-codes.enum';
 
+interface IExceptionBody {
+  message: string;
+  name: string;
+  statusCode: number;
+}
+
 export abstract class HttpException extends Error {
   statusCode: number;
+  body: IExceptionBody;
   constructor(message: string, statusCode: number) {
     super(message);
     this.name = this.constructor.name || 'HttpException';
     this.statusCode = statusCode || HttpStatus.InternalServerError;
+    this.body = this.createBody();
     delete this.stack;
+  }
+  createBody() {
+    return {
+      message: this.message,
+      name: this.name,
+      statusCode: this.statusCode,
+    };
   }
 }
