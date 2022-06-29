411fefcc7d812827f48d8f5723f1a07b8d539174
Roman Avdeenko <btchead@protonmail.ch>
2022-06-29 14:58:26 +0300

    refactor: remove unused validators

--- common/schemas/login.schema.ts
+++ common/schemas/login.schema.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginRequest {
   @IsNotEmpty()
@@ -8,17 +8,5 @@ export class LoginRequest {
   @IsNotEmpty()
   @IsString()
   @MinLength(8, { message: 'password must be longer than 8 characters' })
-  @Matches(/^(?=.*[A-Z].*[A-Z]).{2,}$/m, {
-    message: 'password must contain minimum 2 uppercase letters',
-  })
-  @Matches(/^(?=.*[@!$&#=+-].*[@!$&#=+-]).{2,}$/m, {
-    message: 'password must contain minimum 2 special case letter',
-  })
-  @Matches(/^(?=.*[0-9].*[0-9]).{2,}$/m, {
-    message: 'password must contain minimum 2 digits',
-  })
-  @Matches(/^(?=.*[a-z].*[a-z]).{2,}$/m, {
-    message: 'password must contain minimum 2 lowercase letters',
-  })
   password: string;
 }
